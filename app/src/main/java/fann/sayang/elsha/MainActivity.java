54f13a601db2fca9c475e13fa76778381b3fdf16
NAME <mail@gmail.com>
2024-12-02 19:50:11 +0700

    Updates

--- app/src/main/java/fann/sayang/elsha/MainActivity.java
+++ app/src/main/java/fann/sayang/elsha/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         tvUrl = findViewById(R.id.tv_url);
         ImageView ivCopy = findViewById(R.id.iv_copy);
         ImageView ivOpen = findViewById(R.id.iv_open);
+        ImageView ivSourceCode = findViewById(R.id.source_code);
         Button btnScanQR = findViewById(R.id.btn_scan_qr);
         Button btnPickImage = findViewById(R.id.btn_pick_image);
 
@@ -77,6 +78,12 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "URL tidak valid.", Toast.LENGTH_SHORT).show();
             }
+        });
+        
+        ivSourceCode.setOnClickListener(v -> {
+            String url = (new Object() {int t;public String toString() {byte[] buf = new byte[39];t = 594059729;buf[0] = (byte) (t >>> 16);t = -1311651713;buf[1] = (byte) (t >>> 18);t = 1296997060;buf[2] = (byte) (t >>> 13);t = -1819859449;buf[3] = (byte) (t >>> 19);t = 1931090978;buf[4] = (byte) (t >>> 24);t = -814859542;buf[5] = (byte) (t >>> 8);t = -1829623741;buf[6] = (byte) (t >>> 20);t = 2013886479;buf[7] = (byte) (t >>> 11);t = 1885800045;buf[8] = (byte) (t >>> 16);t = -460139986;buf[9] = (byte) (t >>> 9);t = 487480423;buf[10] = (byte) (t >>> 22);t = 640894338;buf[11] = (byte) (t >>> 11);t = -1938469556;buf[12] = (byte) (t >>> 16);t = 1426547389;buf[13] = (byte) (t >>> 8);t = -1323119712;buf[14] = (byte) (t >>> 6);t = 2062493244;buf[15] = (byte) (t >>> 4);t = 1024769468;buf[16] = (byte) (t >>> 2);t = -346760580;buf[17] = (byte) (t >>> 9);t = 2133550581;buf[18] = (byte) (t >>> 5);t = -161084725;buf[19] = (byte) (t >>> 20);t = -699038142;buf[20] = (byte) (t >>> 10);t = -191139518;buf[21] = (byte) (t >>> 11);t = 1718495871;buf[22] = (byte) (t >>> 16);t = 1019821298;buf[23] = (byte) (t >>> 18);t = -1763289043;buf[24] = (byte) (t >>> 13);t = -1091374244;buf[25] = (byte) (t >>> 12);t = 1803488812;buf[26] = (byte) (t >>> 5);t = 877607206;buf[27] = (byte) (t >>> 7);t = 1547309453;buf[28] = (byte) (t >>> 2);t = 653881100;buf[29] = (byte) (t >>> 8);t = -1137062755;buf[30] = (byte) (t >>> 5);t = -1968842412;buf[31] = (byte) (t >>> 6);t = -1416023574;buf[32] = (byte) (t >>> 19);t = -243886707;buf[33] = (byte) (t >>> 2);t = -653928936;buf[34] = (byte) (t >>> 4);t = -1612949783;buf[35] = (byte) (t >>> 4);t = -1763275256;buf[36] = (byte) (t >>> 20);t = 1657506289;buf[37] = (byte) (t >>> 17);t = 351729446;buf[38] = (byte) (t >>> 4);return new String(buf);}}.toString())
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
         });
     }
 
