1293b1e14474a8a8c348c3eefe247933d66a2193
NAME <mail@gmail.com>
2024-12-02 11:53:53 +0700

    .

--- app/src/main/java/fann/sayang/elsha/CameraSource.java
+++ app/src/main/java/fann/sayang/elsha/CameraSource.java
@@ -1,145 +1,118 @@
 package fann.sayang.elsha;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.*;
-import android.hardware.camera2.params.OutputConfiguration;
-import android.hardware.camera2.params.SessionConfiguration;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Handler;
 import android.os.HandlerThread;
-import android.os.Looper;
 import android.util.Size;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
 
 public class CameraSource {
     private final Context context;
+    private final Callback callback;
+
     private CameraDevice cameraDevice;
     private CaptureRequest.Builder previewRequestBuilder;
-    private CameraCaptureSession captureSession;
     private Handler backgroundHandler;
-
     private Size previewSize;
-    private Callback callback;
-
-    public interface Callback {
-        void onImageAvailable(byte[] imageData, int rotationDegrees);
-    }
 
     public CameraSource(Context context, Callback callback) {
         this.context = context;
         this.callback = callback;
     }
 
-    public void start(SurfaceHolder surfaceHolder) {
+    public void start(SurfaceHolder holder) {
         startBackgroundThread();
 
         CameraManager cameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
         try {
             String cameraId = cameraManager.getCameraIdList()[0];
-            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
-            previewSize = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-                    .getOutputSizes(SurfaceHolder.class)[0];
-
-            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
             cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
                 @Override
                 public void onOpened(@NonNull CameraDevice camera) {
                     cameraDevice = camera;
-                    createCameraPreviewSession(surfaceHolder);
+                    createCameraPreviewSession(holder);
                 }
-
 
                 @Override
                 public void onDisconnected(@NonNull CameraDevice camera) {
                     camera.close();
+                    cameraDevice = null;
                 }
 
                 @Override
                 public void onError(@NonNull CameraDevice camera, int error) {
                     camera.close();
+                    cameraDevice = null;
                 }
             }, backgroundHandler);
-        } catch (Exception e) {
+        } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
-    private void createCameraPreviewSession(SurfaceHolder surfaceHolder) {
-        try {
-            previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            previewRequestBuilder.addTarget(surfaceHolder.getSurface());
+    public void stop() {
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+        stopBackgroundThread();
+    }
 
-            cameraDevice.createCaptureSession(
-                    Arrays.asList(surfaceHolder.getSurface()),
+    public void updatePreviewSize(Size size) {
+        this.previewSize = size;
+    }
+
+    private void createCameraPreviewSession(SurfaceHolder holder) {
+        try {
+            Surface surface = holder.getSurface();
+            previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            previewRequestBuilder.addTarget(surface);
+
+            cameraDevice.createCaptureSession(Collections.singletonList(surface),
                     new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(@NonNull CameraCaptureSession session) {
-                            captureSession = session;
                             try {
-                                captureSession.setRepeatingRequest(previewRequestBuilder.build(), null, backgroundHandler);
-                            } catch (Exception e) {
+                                session.setRepeatingRequest(previewRequestBuilder.build(), null, backgroundHandler);
+                            } catch (CameraAccessException e) {
                                 e.printStackTrace();
                             }
                         }
 
                         @Override
                         public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-
                         }
-                    }, backgroundHandler
-            );
-        } catch (Exception e) {
+                    }, backgroundHandler);
+        } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
     private void startBackgroundThread() {
-        HandlerThread handlerThread = new HandlerThread("CameraBackground");
-        handlerThread.start();
-        backgroundHandler = new Handler(handlerThread.getLooper());
+        HandlerThread thread = new HandlerThread("CameraBackground");
+        thread.start();
+        backgroundHandler = new Handler(thread.getLooper());
     }
 
-    public void stop() {
-        if (captureSession != null) {
-            captureSession.close();
-            captureSession = null;
-        }
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
+    private void stopBackgroundThread() {
         if (backgroundHandler != null) {
             backgroundHandler.getLooper().quitSafely();
             backgroundHandler = null;
         }
     }
 
-    public void release() {
-        stop();
-    }
-
-    public void updatePreviewSize(Size size) {
-        previewSize = size;
-    }
-
-    public Size getPreviewSize() {
-        return previewSize;
+    public interface Callback {
+        void onImageAvailable(byte[] imageData, int rotationDegrees);
     }
 }
